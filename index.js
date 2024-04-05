fde47a084dbd7b1c789e967e339e22d31bba9783
omolo-j <omoloj135@gmail.com>
2024-04-05 22:23:21 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+let max = Math.max(10, 15, 20, 5);
+
+console.log("Random integer greater than 0:", random);
+console.log("Remainder of dividing num3 by num4:", mod);
+console.log("Highest number in the set:", max);
